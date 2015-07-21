249f911df05971d62cf2096c96b647b1f7238280
James Smith <smithjw@users.noreply.github.com>
2015-07-21 13:31:21 +1000

    Create buildkite_tests.sh

---
+++ buildkite_tests.sh
@@ -0,0 +1,13 @@
+# Login to Docker
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+
+# Build image
+docker build -t smithjw/frank-bot:$BUILDBOX_COMMIT .
+
+# Run container with specs
+docker run -e HUBOT_SLACK_TOKEN=xoxb-7834657472-32TecFyNwzjI9UcsMgoTrz3I -d smithjw/frank-bot:$BUILDBOX_COMMIT
+
+# Tag image with current branch name and push when specs are green
+docker tag -f smithjw/frank-bot:$BUILDBOX_COMMIT rsmithjw/frank-bot:$BUILDBOX_BRANCH
+docker push smithjw/frank-bot:$BUILDBOX_BRANCH
+bundle exec rake create_or_update
