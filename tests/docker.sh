584feec6c960c3f4384f4861132d95dea4a07a48
James Smith <mail@smithjw.me>
2015-07-21 14:39:23 +1000

    tests

---
+++ tests/docker.sh
@@ -0,0 +1,14 @@
+sudo apt-get update
+
+# Install Docker and dependencies
+sudo apt-get install docker.io
+sudo ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sudo sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
+
+# Update Docker to latest version
+
+sudo apt-get install apt-transport-https
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sudo sh -c "echo deb https://get.docker.io/ubuntu docker main\ > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get install lxc-docker apparmor
