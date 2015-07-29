4d4b35a71d3a05b7fa0a40568d1492f233fed365
James Smith <smithjw@users.noreply.github.com>
2015-07-29 19:15:31 +1000

    Update buildkite_tests.sh

--- tests/buildkite_tests.sh
+++ tests/buildkite_tests.sh
@@ -1,11 +1,17 @@
+# AWS Login
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set default.region ap-southeast-2
+
 # Build image
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-docker build -t smithjw/frank-bot:$BUILDKITE_COMMIT .
+docker build -t smithjw/doom-bot:$BUILDKITE_COMMIT .
 
 # Run container with specs
-# docker run -e HUBOT_SLACK_TOKEN=xoxb-7834657472-32TecFyNwzjI9UcsMgoTrz3I -d smithjw/frank-bot:$BUILDKITE_COMMIT
+docker run -e HUBOT_SLACK_TOKEN=$HUBOT_SLACK_TOKEN -d smithjw/doom-bot:$BUILDKITE_COMMIT
 
 # Tag image with current branch name and push when specs are green
-# docker tag -f smithjw/frank-bot:$BUILDKITE_COMMIT smithjw/frank-bot:$BUILDKITE_BRANCH
-# docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
-# docker push smithjw/frank-bot:$BUILDKITE_BRANCH
+docker tag -f smithjw/doom-bot:$BUILDKITE_COMMIT smithjw/doom-bot:latest
+docker push smithjw/doom-bot:latest
+
+# Testing
