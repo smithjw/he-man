81899c9742a0fac2c67dd92bba78dcfdfa5483f0
James Smith <smithjw@users.noreply.github.com>
2015-07-30 14:29:16 +1000

    Update buildkite_tests.sh

--- tests/buildkite_tests.sh
+++ tests/buildkite_tests.sh
@@ -1,7 +1,7 @@
 # AWS Login
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-aws configure set default.region ap-southeast-2
+# aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+# aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+# aws configure set default.region ap-southeast-2
 
 # Build image
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
